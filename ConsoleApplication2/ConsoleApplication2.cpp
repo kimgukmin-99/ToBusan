f4a07de0d5c6c825dc196efffe49639489b6a36b
kimgukmin <20180740@gm.hannam.ac.kr>
2024-05-29 01:12:28 +0900

    좀비 시민 마동석 초기설정완료 기차초기설정 완료

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -7,7 +7,6 @@
 #define LEN_MAX		50
 #define PROB_MIN	10
 #define PROB_MAX	90
-
 //마동석
 #define STM_MIN 0 //체력
 #define STM_MAX 5 
@@ -16,17 +15,28 @@
 #define ACTION_REST 0 //행동
 #define ACTION_PROVOKE 1 
 #define ACTION_PULL 2
-
 //좀비
 #define ATK_NONE 0 
 #define ATK_CITIZEN 1
 #define ATK_DONGSEOK 2
-
-#define AGGRO_MIN 0 //어그로 범위
+//어그로 범위
+#define AGGRO_MIN 0 
 #define AGGRO_MAX 5
 
+//마동석 시민 좀비 구조체 생성
+typedef struct {
+	int x;
+	int stamina;
+	int aggro;
+} Madongseok;
+typedef struct {
+	int x;
+	int aggro;
+} Citizen;
 
-
+typedef struct {
+	int x;
+} Zombie;
 
 
 void print_intro(); //인트로 함수
@@ -34,17 +44,30 @@ void print_outtro(bool); //아웃트로 함수
 int set_train_length(int, int); //기차길이 설정
 int set_ma_stamina(int, int); //마동석 체력 결정
 int set_probability(int, int); // 확률 설정
-void print_board(); //보드판 출력
+void print_board(int, Madongseok *, Citizen*, Zombie*); //보드판 출력
+void set_position(int, Madongseok*, Citizen*, Zombie*); //초기위치 설정
+
 
 
 
 int main()
 {
 	print_intro();
-	int train_length;
-	
+	int train_length; //기차길이 입력받기 위한 변수
+	int madongseok_stamina; //마동석 스테미나 입력받는 변수
+	int propability; //확률입력하는 변수
+
+	Madongseok ma{};
+	Citizen cit{};
+	Zombie zom{};
+
+
 	train_length = set_train_length(LEN_MIN, LEN_MAX);
-	printf("%d", train_length);
+	ma.stamina = set_ma_stamina(STM_MIN, STM_MAX);
+	propability = set_probability(PROB_MIN, PROB_MAX);
+	set_position(train_length, &ma, &cit, &zom);
+	print_board(train_length, &ma, &cit, &zom);
+
 	while (1)
 	{
 		break;
@@ -107,6 +130,57 @@ int set_train_length(int min, int max) {
 	}
 	
 }
-void print_board() {
+int set_ma_stamina(int min, int max) {
+	while (1) {
+		int input;
+		printf("madongseok stamina(%d~%d)>> ", min, max);
+		scanf_s("%d", &input);
+		if (input >= min && input <= max) {
+			return input;
+		}
+	}
+}
+int set_probability(int min , int max) {
+	while (1) {
+		int input;
+		printf("percentile probability p(%d~%d)>> ", min, max);
+		scanf_s("%d", &input);
+		if (input >= min && input <= max) {
+			return input;
+		}
+	}
+}
+void print_board(int length, Madongseok *ma, Citizen *cit , Zombie *zom) {
+
+	for (int i = 1; i < length + 1; i++) {
+		printf("#");
+	}
+	printf("\n");
+	printf("#");
+	for (int i = 0; i < length - 2; i++) {
+		if (i == cit->x) {
+			printf("C");
+		}
+		else if (i == zom->x) {
+			printf("Z");
+		}
+		else if (i == ma->x) {
+			printf("M");
+		}
+		else {
+			printf(" ");
+		}
+	}
+	printf("#");
+	printf("\n");
+	for (int i = 0; i < length; i++) {
+		printf("#");
+	}
+	printf("\n");
 
 }
+void set_position(int length, Madongseok *ma, Citizen *cit, Zombie *zom) {
+	ma->x = length - 3;
+	cit->x = length - 7;
+	zom->x = length - 4;
+};
